a7e813445202812ba60273a8023edf8b47389bde
kaajis-kuy <86659971+kaajis-kuy@users.noreply.github.com>
2021-08-09 10:44:29 +0700

    Add files via upload

---
+++ Evaluasi-Sp3/Soal 1.js
@@ -0,0 +1,3 @@
+const santriikhwan = ["sidik","rahmad","shofyan","aldo","kardi"];
+const [a,b,c,d,e,f ]  =Array;
+        console.log(santriikhwan('sidik','rahmad'));
---
+++ Evaluasi-Sp3/Soal 2.js
@@ -0,0 +1,6 @@
+const asalSantri = { "Sidik" : "Lampung", "Wandi" : "Prabumulih", "Rahmad" : "Medan", "Usamah" : "Ceribon", "Syofyan" : "Purwokerto", "Aldi" : "Kendari", "Kardi" : "Lombok" }
+var x={
+   
+}
+
+console.log(asalSantri);
